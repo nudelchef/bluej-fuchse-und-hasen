ee206f73f4a4a420a0f82bd24400ec099cdd0f3d
nudelchef <nudelchef@gmx.de>
2017-10-09 10:25:19 +0200

    Bär added

---
+++ Bär.java
@@ -0,0 +1,33 @@
+
+/**
+ * Beschreiben Sie hier die Klasse Bär.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Bär extends Animal
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    private int x;
+
+    /**
+     * Konstruktor für Objekte der Klasse Bär
+     */
+    public Bär()
+    {
+        // Instanzvariable initialisieren
+        x = 0;
+    }
+
+    /**
+     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
+     * 
+     * @param  y    ein Beispielparameter für eine Methode
+     * @return        die Summe aus x und y
+     */
+    public int beispielMethode(int y)
+    {
+        // tragen Sie hier den Code ein
+        return x + y;
+    }
+}
